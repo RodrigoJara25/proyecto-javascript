6b64a4282386122d7ae1ba94c87e4881c3b38f4a
RodrigoJara25 <r.jara.e7@gmail.com>
2024-12-19 19:51:19 -0500

    class producto y categoria

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,75 @@
 console.log("Empezamos el sistema")
 
-function ingresarProducto() {
-    let id = prompt("Ingresar id del producto")
-    let nombre = prompt("Ingresar nombre del producto")
-    let cantidad = parseInt(prompt("Ingresar cantidad del producto"))
-    let precio = parseInt(prompt("Ingresar precio del producto"))
-    alert("Informacion del producto ingresado: \n" + 
-            "id: " + id + "\n" +
-            "nombre: " + nombre + "\n" +
-            "cantidad: " + cantidad + "\n" +
-            "precio: " + precio + "\n")
-    console.log("Informacion del producto ingresado: \n" + 
-            "id: " + id + "\n" +
-            "nombre: " + nombre + "\n" +
-            "cantidad: " + cantidad + "\n" +
-            "precio: " + precio + "\n" + "\n")
-    // return id, nombre, cantidad, precio
+// ============================= CLASES =============================
+// Clase Producto (id, nombre, cantidad, precio)
+class Producto{
+    // Propiedad estatica para llevar la cuenta automatica de los id
+    static id = 0;
+
+    // Inicializacion
+    nombre = "";        // string
+    cantidad = 0;       // int
+    precio = 0;         // float
+
+    // Constructor
+    constructor(nombre, cantidad, precio) {
+        // Incrementamos el id automaticamente
+        this.id = Producto.id++;
+        // Asiganmos los datos
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    // Metodos
 }
 
-function eliminarProducto(){
-    let id = prompt("Ingresar id del producto a eliminar")
-    id = null
-    nombre = null
-    cantidad = null
-    precio = null
-    alert("Producto eliminado")
+// Clase Categoria (nombre)
+class Categoria {
+    // Inicializacion
+    nombre = "";
+    array_productos = [];
+
+    // Constructor
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+
+    // Metodos
+
+    // Ingresar producto en esta categoria
+    ingresarProducto() {
+        let nombre = prompt("Ingresar nombre del producto");
+        let cantidad = parseInt(prompt("Ingresar cantidad del producto"));
+        let precio = parseInt(prompt("Ingresar precio del producto"));
+        const producto = new Producto(nombre, cantidad, precio);
+        this.array_productos.push(producto)
+
+        // Mostrar la informacion del producto agregado
+        console.log("Informacion del producto ingresado: \n" + 
+                "id: " + producto.id + "\n" +
+                "nombre: " + producto.nombre + "\n" +
+                "cantidad: " + producto.cantidad + "\n" +
+                "precio: " + producto.precio + "\n" + "\n")
+    }
+    
+    // Eliminar un Porducto de la Categoria (se borra de todos lados)
+    eliminarProducto(){
+        let id = prompt("Ingresar id del producto a eliminar");
+        const productosActualizados = this.array_productos.filter((producto) => producto.id != id);
+        this.array_productos = productosActualizados;
+
+        // Mostrar los productos que quedan en esta categoria
+        for (const producto of this.array_productos) {
+            console.log(producto)
+        }
+        alert("Producto eliminado")
+    }
 }
 
+// ============================= VARIABLES USADAS =============================
+
+/*
 function agregarStock(){
     let id = prompt("Ingresar el id del producto a aumentar stock")
     let cantidad_agregar = parseInt(prompt("Cantidad agregada"))
@@ -96,3 +139,11 @@ if (password_ingresada === password_real) {
 } else {
     alert("Limite de intentos excedido")
 }
+ */
+
+let nom_categoria = prompt("Ingresar el nombre de la categoria: ");
+const cat_frutas = new Categoria("nom_categoria");
+cat_frutas.ingresarProducto()
+cat_frutas.ingresarProducto()
+cat_frutas.eliminarProducto()
+
