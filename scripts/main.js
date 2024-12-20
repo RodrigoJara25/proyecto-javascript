d18289d983e1586941f5c3b31cc7c0181c883c94
RodrigoJara25 <r.jara.e7@gmail.com>
2024-12-20 10:30:59 -0500

    avance 2da entrega

--- scripts/main.js
+++ scripts/main.js
@@ -37,12 +37,9 @@ class Categoria {
 
     // Metodos
 
-    // Ingresar producto en esta categoria
-    ingresarProducto() {
-        let nombre = prompt("Ingresar nombre del producto");
-        let cantidad = parseInt(prompt("Ingresar cantidad del producto"));
-        let precio = parseInt(prompt("Ingresar precio del producto"));
-        const producto = new Producto(nombre, cantidad, precio);
+    // Ingresar producto en esta categoria (crearlo defrente)
+    ingresarProducto(producto) {
+        // Agregamos el producto al array de esta categoria
         this.array_productos.push(producto)
 
         // Mostrar la informacion del producto agregado
@@ -53,41 +50,124 @@ class Categoria {
                 "precio: " + producto.precio + "\n" + "\n")
     }
     
-    // Eliminar un Porducto de la Categoria (se borra de todos lados)
+    // Eliminar un Porducto de la Categoria 
     eliminarProducto(){
-        let id = prompt("Ingresar id del producto a eliminar");
+        let id = parseInt(prompt("Ingresar id del producto a eliminar"));
+        const productoEncontrado = this.array_productos.some((producto) => producto.id == id);
         const productosActualizados = this.array_productos.filter((producto) => producto.id != id);
         this.array_productos = productosActualizados;
-
-        // Mostrar los productos que quedan en esta categoria
-        for (const producto of this.array_productos) {
-            console.log(producto)
+        if (!productoEncontrado) {
+            alert("Producto no encontrado")
+        } else{
+            // Mostrar los productos que quedan en esta categoria
+            for (const producto of this.array_productos) {
+                console.log(producto)
+            }
+            alert("Producto eliminado")
         }
-        alert("Producto eliminado")
+    }
+
+    // Agregar Stock de un Producto
+    agregarStock(){
+        let id = parseInt(prompt("Ingresar el id del producto a aumentar stock"))
+        const producto_agregar_stock = this.array_productos.find((producto) => producto.id === id)
+        if (producto_agregar_stock == undefined) {
+            alert("Producto no encontrado")
+        } else {
+            let cantidad_agregar = parseInt(prompt("¿Qué cantidad desea agregar?"))
+            if (isNaN(cantidad_agregar)) {
+                alert("Dato invalido");
+            } else {
+                producto_agregar_stock.cantidad = producto_agregar_stock.cantidad + cantidad_agregar; 
+                alert("Stock agregado")
+                console.log(producto_agregar_stock)
+            }
+        }
+    }
+
+    // Modificar el Precio de un Producto
+    modificarPrecio(){
+        let id = parseInt(prompt("Ingresar el id del producto a modificar precio"))
+        const producto_modificar_precio = this.array_productos.find((producto) => producto.id === id)
+        if (producto_modificar_precio == undefined) {
+            alert("Producto no encontrado")
+        } else{
+            let precio = parseFloat(prompt("Ingresa el Nuevo Precio"));
+            if (isNaN(precio)) {
+                alert("Precio invalido")
+            } else {
+                producto_modificar_precio.precio = precio; 
+                alert("Precio modificado")   
+                
+                // Verificar
+                console.log(producto_modificar_precio);
+            }
+        }
+    }
+
+    // Modificar el nombre de un producto
+    modificarNombre(){
+        let id = parseInt(prompt("Ingresar el id del producto a modificar el nombre"));
+        const producto_modificar_nombre = this.array_productos.find((producto) => producto.id === id)
+        if (producto_modificar_nombre == undefined) {
+            alert("Producto no encontrado")
+        } else{
+            let nombre = prompt("Ingrese el Nuevo Nombre")
+            producto_modificar_nombre.nombre = nombre;
+            alert("Nombre modificado")
+
+            // verificar en consola
+            console.log(producto_modificar_nombre);
+        }
     }
 }
 
-// ============================= VARIABLES USADAS =============================
+// Clase Pedido
+class Pedido {
+    // Inicializacion
+    cantidad = 0;
+    nombre_producto = "";
+    descripcion = "";
+    precio_unitario = 0;
+    total = ""
+
+    constructor(cantidad, nombre_producto, precio_unitario){
+        
+    }
+}
+
+// Clase Factura
+class Factura {
+    // Inicializacion 
+    num_boleta = 0;
+    fecha_emision = ""
+    nombre_empresa = ""
+    ruc = ""
+    dni_cliente = "000000000"
+    nombre_cliente = ""
+    
+}
+
+
+
+// ============================= FUNCIONES =============================
+// Crear un Producto
+function crearProducto(){
+    let nombre = prompt("Ingresar nombre del producto");
+    let cantidad = parseInt(prompt("Ingresar cantidad del producto"));
+    let precio = parseInt(prompt("Ingresar precio del producto"));
+    const producto = new Producto(nombre, cantidad, precio);
+    return producto;
+}
+
+// Crear Categoria de Producto
+function crearCategoria(){
+    let nombre = prompt("Ingresar nombre de la categoria del producto");
+    const categoria = new Categoria(nombre);
+    return categoria;
+}
 
 /*
-function agregarStock(){
-    let id = prompt("Ingresar el id del producto a aumentar stock")
-    let cantidad_agregar = parseInt(prompt("Cantidad agregada"))
-    alert("Stock agregado")
-}
-
-function modificarPrecio(){
-    let id = prompt("Ingresar el id del producto a modificar precio")
-    let precio = parseInt(prompt("Nuevo precio"))
-    alert("Precio modificado")
-}
-
-function modificarNombre(){
-    let id = prompt("Ingresar el id del producto a modificar el nombre")
-    let nombre = prompt("Nuevo nombre")
-    alert("Nombre modificado")
-}
-
 alert("Bienvenido al sistema de inventario")
 let limite_intentos = 3
 let intento_actual = 0
@@ -141,9 +221,12 @@ if (password_ingresada === password_real) {
 }
  */
 
-let nom_categoria = prompt("Ingresar el nombre de la categoria: ");
-const cat_frutas = new Categoria("nom_categoria");
-cat_frutas.ingresarProducto()
-cat_frutas.ingresarProducto()
-cat_frutas.eliminarProducto()
+const cat_frutas = new Categoria("Frutas");
+const producto1 = new Producto("Manzana", 5, 5);
+const producto2 = new Producto("Pera", 7, 7);
+const producto3 = new Producto("Durazno", 2, 2);
+cat_frutas.ingresarProducto(producto1);
+cat_frutas.ingresarProducto(producto2);
+cat_frutas.ingresarProducto(producto3);
+// cat_frutas.modificarNombre();
 
