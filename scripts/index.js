15e1183697595a7815874bd3a6ff0e1bad5d1387
RodrigoJara25 <r.jara.e7@gmail.com>
2025-02-21 23:12:01 -0500

    css mejorado

--- scripts/index.js
+++ scripts/index.js
@@ -59,10 +59,6 @@ let array_categorias = []
 let array_productos = []
 
 // ============================= EJECUCION DEL CODIGO =============================
-
-// Seleccionamos el formulario donde se marca la opcion que se quiere hacer
-let formularioOpcion = document.getElementById("formulario-opcion")
-
 // Preparamos el formulario que se va a generar segun la opcion que se elija de manera dinamica
 let contenedorNuevoFormulario = document.getElementById("formulario-opciones");
 
@@ -74,16 +70,13 @@ fetch('/mocks/productos.json')
     .then((response) => response.json())
     .then((data) => {
         cargarProductos();
-        console.log(array_productos);
         // let array_productos_JSON = data.map(prod => new Producto(prod.nombre, prod.cantidad, prod.precio));
         let array_productos_JSON = data;
-        console.log("array_productos_JSON: ", array_productos_JSON);
         array_productos_JSON.forEach((producto_json)=>{
             if (!array_productos.some(producto_storage => producto_storage.nombre === producto_json.nombre)) {
                 array_productos.push(new Producto(producto_json.nombre, producto_json.cantidad, producto_json.precio))
             }
         })
-        console.log("--> Datos decodificados <--", data);
         /* {
         "id": 0,
         "nombre": "Manzana",
@@ -124,7 +117,6 @@ fetch('/mocks/categorias.json')
                 array_categorias.push(categoria_json)
             }
         })
-        console.log("--> Datos decodificados <--", data);
         /* {
         "nombre": "Frutas",
         "array_productos": [
@@ -149,66 +141,68 @@ fetch('/mocks/categorias.json')
             title: "?",
             text: "A ocurrido error al cargar las categorias",
             icon: "question"
-          });
+        });
     })
     .finally(()=>{
         guardarCategorias();
     });
 
-// Cuando el formulario de opciones sea enviado (submit) verificamos cual fue la opcion enviada
-formularioOpcion.addEventListener("submit", (event)=>{
+// Creamos un formulario cada vez que se seleccione una opcion
+let formulario = document.createElement("form");
+formulario.setAttribute("id", "formulario-dinamico")
+contenedorNuevoFormulario.append(formulario);
+let form = ""
+
+// Resetamos e; html de los fomrularios
+const btnReset = document.getElementById("btnReset");
+btnReset.addEventListener("click", (event)=>{
     event.preventDefault();
+    contenedorNuevoFormulario.innerHTML = "";
+})
 
-    // Creamos un formulario cada vez que se seleccione una opcion
-    let formulario = document.createElement("form");
-    formulario.setAttribute("id", "formulario-dinamico")
+// Recibo todos los botones de opciones
+const btnCrearCategoria = document.getElementById("crearCategoria");
+const btnCrearProducto = document.getElementById("crearProducto");
+const btnAgregarProducto = document.getElementById("agregarProducto");
+const btnEliminarProducto = document.getElementById("eliminarProducto");
+
+btnCrearCategoria.addEventListener("click", (e)=>{
+    e.preventDefault();
+    // Asignamos el formulario dentro del contenedor (que es la section)
     contenedorNuevoFormulario.append(formulario);
-    let form = ""
+    form = document.getElementById("formulario-dinamico")
+    formulario.innerHTML = `<label for="nombre-categoria">Nombre de la Categoría</label>
+                            <input type="text" id="nombre-categoria" name="nombre-categoria" placeholder="Ingrese el nombre de la categoría" required>
+                            <button type="submit">Guardar</button>`
+    // Cuando ese nuevo formulario dinamico sea enviado (submit), creamos la categoria con sus datos
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        let categoria = crearCategoria();
+        array_categorias.push(categoria);
+        formulario.remove();    // eliminamos el formulario luego de usarlo para que se vuyelva a generar cuando sea encesario
 
-    let opcion = document.getElementById("opcion").value;   // Agarramos el valor del input (que va a ser el numero de la opcion)
+        // Actualizamos los arrays de productos y categorias en el localStorage
+        guardarProductos();
+        guardarCategorias();
 
-    // Si se presiona "Resetear" se elimina todo lo generado
-    formularioOpcion.addEventListener("reset", (event)=>{
-        contenedorNuevoFormulario.innerHTML = "";
+        // Recargamos el HTML
+        mostrarInterfazProductos();
+        mostrarInterfazCategorias();
+
+        // Mensaje de confirmacion
+        Toastify({
+            text: "Categoria creada",
+            duration: 3000,
+            gravity: "top",
+            position: "right",
+            backgroundColor: "red",
+        }).showToast();
     })
+})
 
-    switch (opcion) {
-        // Si es la opcion "1", entonces creamos un formulario dentro del contenedor formulario con los campos necesarios (nombre de la categoria)
-        case "1":   // agregar categoria
-            // Asignamos el formulario dentro del contenedor (que es la section)
-            contenedorNuevoFormulario.append(formulario);
-            form = document.getElementById("formulario-dinamico")
-            formulario.innerHTML = `<label for="nombre-categoria">Nombre de la Categoría</label>
-                                    <input type="text" id="nombre-categoria" name="nombre-categoria" placeholder="Ingrese el nombre de la categoría" required>
-                                    <button type="submit">Guardar</button>`
-            // Cuando ese nuevo formulario dinamico sea enviado (submit), creamos la categoria con sus datos
-            form.addEventListener("submit", (event) => {
-                event.preventDefault()
-                let categoria = crearCategoria();
-                array_categorias.push(categoria);
-                console.log("Categoria agregada:" + categoria?.nombre)
-                formulario.remove();    // eliminamos el formulario luego de usarlo para que se vuyelva a generar cuando sea encesario
-
-                // Actualizamos los arrays de productos y categorias en el localStorage
-                guardarProductos();
-                guardarCategorias();
-
-                // Recargamos el HTML
-                mostrarInterfazProductos();
-                mostrarInterfazCategorias();
-
-                // Mensaje de confirmacion
-                Toastify({
-                    text: "Categoria creada",
-                    duration: 3000,
-                    gravity: "top",
-                    position: "right",
-                    backgroundColor: "red",
-                }).showToast();
-            })
-            break;
-        case "2":   // agregar producto
-            contenedorNuevoFormulario.append(formulario);
+btnCrearProducto.addEventListener("click", (e) => {
+    e.preventDefault();
+    contenedorNuevoFormulario.append(formulario);
             form = document.getElementById("formulario-dinamico")
             formulario.innerHTML = `
                 <label for="nombre-producto">Nombre del Producto</label>
@@ -226,9 +220,6 @@ formularioOpcion.addEventListener("submit", (event)=>{
                 event.preventDefault()
                 let producto = crearProducto();
                 array_productos.push(producto);
-                console.log("Producto agregado:" + producto?.nombre)
-                console.log("Cantidad:" + producto?.cantidad)
-                console.log("Precio:" + producto?.precio)
                 Toastify({
                     text: "Producto creado",
                     duration: 3000,
@@ -245,93 +236,93 @@ formularioOpcion.addEventListener("submit", (event)=>{
                 mostrarInterfazProductos();
                 mostrarInterfazCategorias();
             })
-            break;
-        case "3":       
-            // Crear el título de productos
-            let tituloProductos = document.createElement("h2");
-            tituloProductos.textContent = "Productos";
-            contenedorNuevoFormulario.append(tituloProductos);
+})
 
-            let listaProductos = document.createElement("ul");
-            contenedorNuevoFormulario.append(listaProductos);
-        
-            array_productos.forEach(producto => {
-                listaProductos.innerHTML += `<li class="productos">${producto.informacionProducto()}</li>`;
-            });
-        
-            // Crear el título de categorías
-            let tituloCategorias = document.createElement("h2");
-            tituloCategorias.textContent = "Categorías";
-            contenedorNuevoFormulario.append(tituloCategorias);
+btnAgregarProducto.addEventListener("click", (e)=>{
+    e.preventDefault();
+    // Crear el título de productos
+    let tituloProductos = document.createElement("h2");
+    tituloProductos.textContent = "Productos";
+    contenedorNuevoFormulario.append(tituloProductos);
 
-            let listaCategorias = document.createElement("ul");
-            contenedorNuevoFormulario.append(listaCategorias);
-        
-            array_categorias.forEach(categoria => {
-                listaCategorias.innerHTML += `<li class="categorias">${categoria.nombre}</li>`;
-            });
-        
-            // Crear el formulario
-            contenedorNuevoFormulario.append(formulario);
-            form = document.getElementById("formulario-dinamico");
-        
-            formulario.innerHTML = `
-                <br>
-                <p>¿Qué producto quieres agregar?</p>
-                <label for="nombre-producto">Nombre del Producto</label>
-                <input type="text" id="nombre-producto" name="nombre-producto" placeholder="Ingrese el nombre del producto" required><br>
-        
-                <br>
-                <p>¿A qué categoría deseas agregar?</p>
-                <br>
-                <label for="nombre-categoria">Nombre de la Categoria</label>
-                <input type="text" id="nombre-categoria" name="nombre-categoria" placeholder="Ingrese el nombre de la categoria" required><br>
-        
-                <button type="submit">Guardar</button>
-            `;
-        
-            form.addEventListener("submit", (event) => {
-                event.preventDefault();
-                let nombre_producto = document.getElementById("nombre-producto").value;
-                let producto_elegido = array_productos.find((producto) => producto.nombre == nombre_producto);
-        
-                let nombre_categoria = document.getElementById("nombre-categoria").value;
-                let categoria_elegida = array_categorias.find((categoria) => categoria.nombre == nombre_categoria);
+    let listaProductos = document.createElement("ul");
+    contenedorNuevoFormulario.append(listaProductos);
 
-                (!producto_elegido || !categoria_elegida)  
-                    ? Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text: "Categoría o Producto no existente!"
-                        })  
-                    : categoria_elegida.ingresarProducto(producto_elegido);
+    array_productos.forEach(producto => {
+        listaProductos.innerHTML += `<li class="productos">${producto.informacionProducto()}</li>`;
+    });
+
+    // Crear el título de categorías
+    let tituloCategorias = document.createElement("h2");
+    tituloCategorias.textContent = "Categorías";
+    contenedorNuevoFormulario.append(tituloCategorias);
+
+    let listaCategorias = document.createElement("ul");
+    contenedorNuevoFormulario.append(listaCategorias);
+
+    array_categorias.forEach(categoria => {
+        listaCategorias.innerHTML += `<li class="categorias">${categoria.nombre}</li>`;
+    });
+
+    // Crear el formulario
+    contenedorNuevoFormulario.append(formulario);
+    form = document.getElementById("formulario-dinamico");
+
+    formulario.innerHTML = `
+        <br>
+        <p>¿Qué producto quieres agregar?</p>
+        <label for="nombre-producto">Nombre del Producto</label>
+        <input type="text" id="nombre-producto" name="nombre-producto" placeholder="Ingrese el nombre del producto" required><br>
+
+        <br>
+        <p>¿A qué categoría deseas agregar?</p>
+        <br>
+        <label for="nombre-categoria">Nombre de la Categoria</label>
+        <input type="text" id="nombre-categoria" name="nombre-categoria" placeholder="Ingrese el nombre de la categoria" required><br>
+
+        <button type="submit">Guardar</button>
+    `;
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let nombre_producto = document.getElementById("nombre-producto").value;
+        let producto_elegido = array_productos.find((producto) => producto.nombre == nombre_producto);
+
+        let nombre_categoria = document.getElementById("nombre-categoria").value;
+        let categoria_elegida = array_categorias.find((categoria) => categoria.nombre == nombre_categoria);
+
+        (!producto_elegido || !categoria_elegida)  
+            ? Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Categoría o Producto no existente!"
+                })  
+            : categoria_elegida.ingresarProducto(producto_elegido);
 
 
-                formulario.remove();  // Eliminamos el formulario luego de usarlo para que se vuelva a generar cuando sea necesario
-                listaCategorias.remove();
-                tituloCategorias.remove();
-                listaProductos.remove();
-                tituloProductos.remove();
+        formulario.remove();  // Eliminamos el formulario luego de usarlo para que se vuelva a generar cuando sea necesario
+        listaCategorias.remove();
+        tituloCategorias.remove();
+        listaProductos.remove();
+        tituloProductos.remove();
 
-                // Actualizamos los arrays de productos y categorias
-                guardarProductos();
-                guardarCategorias();
-                // Recargamos el HTML
-                mostrarInterfazProductos();
-                mostrarInterfazCategorias();
-            });
-            
-            break;
-        case  "4":      // Eliminar producto
-            // debugger
-            let listaCategorias2 = document.createElement("ul") 
+        guardarProductos();
+        guardarCategorias();
+        
+        mostrarInterfazProductos();
+        mostrarInterfazCategorias();
+    });
+})
+
+btnEliminarProducto.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let listaCategorias2 = document.createElement("ul") 
             contenedorNuevoFormulario.append(listaCategorias2);
         
             array_categorias.forEach(categoria => {
                 listaCategorias2.innerHTML += `<li class="categorias">${categoria.nombre}</li>`;
             });
 
-            // Crear el formulario
             contenedorNuevoFormulario.append(formulario);
             form = document.getElementById("formulario-dinamico");
             formulario.innerHTML = `
@@ -341,7 +332,6 @@ formularioOpcion.addEventListener("submit", (event)=>{
                 <input type="text" id="nombre-categoria" name="nombre-categoria" placeholder="Ingrese el nombre de la categoria" required><br>
                 <button type="submit">Guardar</button>
             `;
-            // debugger
             form.addEventListener("submit", (event) => {
                 event.preventDefault();
                 nombre_categoria = document.getElementById("nombre-categoria").value;
@@ -356,16 +346,6 @@ formularioOpcion.addEventListener("submit", (event)=>{
                     categoria_elegida.eliminarProducto()
                     listaCategorias2.remove()   
                 }
-                formulario.remove();  // Eliminamos el formulario luego de usarlo para que se vuelva a generar cuando sea necesario
+                formulario.remove(); 
             });
-            break;
-        default:
-            Swal.fire({
-                icon: "error",
-                title: "Ocurrió un error...",
-                text: "Opción no disponible!"
-                })
-            break;
-    }
-
 })
